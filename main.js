6942a60fcde91edeac4f2e349d5a395458139949
Nathan Youngman <4566+nathany@users.noreply.github.com>
2019-02-13 14:25:18 -0700

    More resolution for frequency visualizer (zoom in + mini)

--- main.js
+++ main.js
@@ -1,30 +1,44 @@
 let analyser = null;
 let visualizer = null;
+let minimap = null;
 
-const barCount = 32;
+const barCount = 256;
 
-let barColor = null;
-const colorListening = 'rgb(200,200,50)';
+const colorMini = 'rgb(200,50,50)';
+const colorFull = 'rgb(200,200,50)';
 const colorBackground = 'rgb(0, 0, 0)';
 const labelColor = 'rgb(255, 255, 255)';
 const labelFont = '14px sans-serif';
 
 class Visualizer {
-  constructor(canvasID) {
+  constructor(canvasID, mini) {
     const canvas = document.getElementById(canvasID);
     this.width = canvas.width;
     this.height = canvas.height;
     this.ctx = canvas.getContext('2d');
-    this.graphOffsetX = 40;
-    this.graphReserveY = 20;
+    if (mini) {
+      this.barWidth = 1;
+      this.paddedBarWidth = 1;
+      this.graphOffsetX = 0;
+      this.graphReserveY = 0;
+      this.barColor = colorMini;
+    } else {
+      this.barWidth = 31;
+      this.paddedBarWidth = 32;
+      this.graphOffsetX = 40;
+      this.graphReserveY = 20;
+      this.barColor = colorFull;
+    }
     this.graphWidth = this.width - this.graphOffsetX;
     this.graphHeight = this.height - this.graphReserveY;
 
     this.ctx.fillStyle = colorBackground;
     this.ctx.fillRect(0, 0, this.width, this.height);
 
-    this.dBAxis(-100, -30);
-    this.freqAxis();
+    if(!mini) {
+      this.dBAxis(-100, -30);
+      this.freqAxis();
+    }
   }
 
   clear() {
@@ -51,31 +65,29 @@ class Visualizer {
     this.ctx.font = labelFont;
     this.ctx.fillStyle = labelColor;
 
-    const paddedBarWidth = this.graphWidth / barCount;
-
     let x = 0;
     let hz = 0;
     // 0 hertz to 22,050 hertz
     for(let b = 0; b < barCount; b++) {
-      hz = Math.ceil(b / barCount * 22)
-      this.ctx.fillText(hz.toString(), this.graphOffsetX + x, this.height - 2);
-      x += paddedBarWidth;
+      hz = b / barCount * 22;
+      this.ctx.fillText(hz.toFixed(1), this.graphOffsetX + x, this.height - 2);
+      x += this.paddedBarWidth;
     }
   }
 
   graph(dataArray) {
     this.clear();
 
-    const barWidth = (this.graphWidth / barCount) - 1;
+    const barWidth = 32 - 1; // (this.graphWidth / barCount) - 1;
 
     let barHeight = 0;
     let x = this.graphOffsetX;
-    this.ctx.fillStyle = barColor;
+    this.ctx.fillStyle = this.barColor;
 
     for (let f of dataArray) {
       barHeight = f * (this.height - this.graphReserveY) / 256;
-      this.ctx.fillRect(x, this.height-this.graphReserveY-barHeight, barWidth, barHeight);
-      x += barWidth + 1;
+      this.ctx.fillRect(x, this.height-this.graphReserveY-barHeight, this.barWidth, barHeight);
+      x += this.paddedBarWidth;
     }
   }
 }
@@ -130,12 +142,14 @@ function visualize() {
   requestAnimationFrame(visualize);
   const dataArray = analyser.getFrequencyData();
   visualizer.graph(dataArray);
+  minimap.graph(dataArray);
 }
 
 window.onload = function () {
   const audioControl = document.getElementById('audio');
   const micButton = document.getElementById('microphone');
-  visualizer = new Visualizer('canvas');
+  visualizer = new Visualizer('canvas', false);
+  minimap = new Visualizer('minimap', true);
 
   micButton.addEventListener('click', async () => {
     analyser = new Analyser();
@@ -144,7 +158,6 @@ window.onload = function () {
     micButton.style.display = 'none';
     document.getElementById('interface').style.display = 'block';
     analyser.fromStream(microphone);
-    barColor = colorListening;
     visualize();
   });
 }
